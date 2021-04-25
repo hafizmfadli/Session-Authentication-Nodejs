15c0ea801519db9a1fe22b9e93d235e5169e29d9
hafizmfadli <lshafizmf1st@gmail.com>
2021-04-25 09:27:09 +0700

    Configure database

---
+++ db/index.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg')
+const user = process.env.PGUSER
+const password = process.env.PGPASSWORD
+const host = process.env.PGHOST
+const database = process.env.PGDATABASE
+const port = process.env.PGPORT
+
+const connectionString = `postgresql://${user}:${password}@${host}:${port}/${database}`
+
+const pool = new Pool({
+    connectionString
+})
+
+module.exports = {
+    query: (text, params) => pool.query(text, params)
+}
