fedc28ba07a716ff293fb207bfdc890dcbe1eb9c
hafizmfadli <lshafizmf1st@gmail.com>
2021-04-25 22:19:19 +0700

    Move all route and import it as middleware

--- index.js
+++ index.js
@@ -3,36 +3,18 @@ require('dotenv').config()
 const express = require('express')
 const session = require('./middleware/session')
 const userDAO = require('./dao/user')
+const routes = require('./routes/route')
+
 const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 
-// session middleware
+// middleware
+app.use(express.json())
 app.use(session)
-
-// endpoint dummy untuk bikin session
-app.get('/session', (req, res) => {
-    req.session.lastmodifed = new Date()
-    res.end()
-})
-
-app.get('/', (req, res) => {
-    res.render('pages/index')
-})
-
-app.get('/login', (req, res) => {
-    res.render('pages/login')
-})
-
-app.get('/signup', (req, res) => {
-    res.render('pages/signup')
-})
-
-app.get('/product', (req, res) => {
-    res.render('pages/product')
-})
+app.use(routes)
 
 app.listen(PORT, async() => {
     try {
