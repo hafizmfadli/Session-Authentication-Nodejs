b57085971675aa30f4d5e86af9b2ad05f16d4a93
hafizmfadli <lshafizmf1st@gmail.com>
2021-04-25 09:27:37 +0700

    Add DAO for user

---
+++ dao/user.js
@@ -0,0 +1,21 @@
+const db = require('../db/index')
+
+const createUserTable = async () => {
+    const sql = `
+    CREATE TABLE IF NOT EXISTS users (
+        user_id serial PRIMARY KEY,
+        email VARCHAR ( 255 ) UNIQUE NOT NULL,
+        password VARCHAR ( 255 ) NOT NULL 
+    );`
+    try {
+        await db.query(sql)
+        console.log("User table berhasil dibuat")    
+        return Promise.resolve()
+    } catch (error) {
+        return Promise.reject(new Error("User table gagal dibuat"))        
+    }    
+}
+
+module.exports = {
+    createUserTable
+}
