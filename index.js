58656495f026886e0227193932b1a4757eca3346
hafizmfadli <lshafizmf1st@gmail.com>
2021-04-25 07:45:12 +0700

    Set up session middleware
    Set up session middleware dan menyimpan session secret di env variabel

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+require('dotenv').config()
 const express = require('express')
+const { MemoryStore } = require('express-session')
+const session = require('express-session')
 const app = express()
 const PORT = process.env.PORT || 8000
 
+const sessionStorage = new MemoryStore()
+
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
+
+// session middleware
+app.use(session({
+    name: 'sessionid',
+    secret: process.env.SESSION_SECRET,
+    store: sessionStorage,
+    saveUninitialized: false,
+    resave: false,
+    rolling: true,
+    cookie: {
+        secure: false,
+        httpOnly: true,
+        maxAge: 1000 * 60
+    }
+}))
 
 app.get('/', (req, res) => {
     res.render('pages/index')
@@ -21,4 +41,4 @@ app.get('/product', (req, res) => {
     res.render('pages/product')
 })
 
-app.listen(PORT, () => console.log(`LISTEN ${PORT}`))
+app.listen(PORT, () => console.log(`LISTEN ${PORT} SESSION_SECRET`))
