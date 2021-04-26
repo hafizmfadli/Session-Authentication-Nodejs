f07ca5d62dabf82a90b8fe9bd51ae4fd8efb390e
hafizmfadli <lshafizmf1st@gmail.com>
2021-04-26 10:29:11 +0700

    Add logic for login

--- service/userService.js
+++ service/userService.js
@@ -13,10 +13,30 @@ const signup = async (email, password) => {
         const user = await userDAO.insertOneUser(email, hash)
         return user
     } catch (error) {
-        return Promise.reject(new Error('signup failed'))    
+        return Promise.reject(error)    
     }
 }
 
+const login = async (email, password) => {
+    // 1. validasi email (sudah dilakukan di middleware)
+    const error = new Error()
+
+    try {
+        // 2. find user
+        const user = await userDAO.findUserByEmail(email)
+        const match = await bcrypt.compare(password, user.password)
+        if(match){
+            return user
+        }
+        error.status = 400
+        error.message = 'password salah'
+        throw error
+    } catch (error) {
+        return Promise.reject(error)
+    }
+} 
+
 module.exports = {
-    signup    
+    signup,
+    login    
 }
