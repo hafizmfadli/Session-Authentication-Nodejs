51d07b03db22f5df86817651196704a294e2a441
hafizmfadli <lshafizmf1st@gmail.com>
2021-04-26 14:57:59 +0700

    change behaviour for handle unauth users

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,10 @@
 const checkAuth = (req, res, next) => {
     if(!req.session || !req.session.user){
-        const error = new Error('You need to login first')
-        error.statusCode = 401
-        next(error)
+        // const error = new Error('You need to login first')
+        // error.statusCode = 401
+        // next(error)
+        
+        res.redirect('/login')
     }
     next()
 }
